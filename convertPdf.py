2c6ab20ad1727c21fa06004c83689942ac9cb51b
DESKTOP-DLM7B21\Cimatec <lucas.jordi@fbter.org.br>
2021-12-13 15:43:09 -0300

    13/12/2021 15:43

--- convertPdf.py
+++ convertPdf.py
@@ -7,48 +7,7 @@ with open('README.md', 'r', encoding="utf8") as f:
     html = markdown.markdown(text)
 
 with open('Manual_Cloud_Foundry(MindSphere).html', 'w',encoding="utf8") as f:
-    f.write("""<style>
-  @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;1,100&display=swap');
-  body{
-    font-family: 'Roboto', sans-serif;;
-    font-size: 14px;
-    line-height: 1.5;
-    background-color: var(--color-canvas-default);
-    margin-left:5%;
-    word-wrap: break-word;    
-  }
-  h1{
-    margin-top:2%;
-    text-align:center;
-  }
-  h2{
-    margin-top:5%
-  }
-  a{    
-    text-decoration: none;
-  }
-  li{
-    margin-bottom:2%;
-    margin-top:2%
-  }
-  ol{
-    margin-bottom:5%
-  }
-  figure{
-    margin-bottom:5%;
-    margin-top:5%;
-    width:80%
-  }
-  figcaption{
-    font-size:12px;
-    font-weight:bold
-  }
-  img{
-    width:80%
-  }
-  
-
-</style>  """+html)
+    f.write(html)
 
 
 ### Para transformar html em pdf executar comando wkhtmltopdf --enable-local-file-access Manual_Cloud_Foundry(MindSphere).html Manual_Cloud_Foundry(MindSphere).pdf
